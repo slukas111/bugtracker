510f52fe86f39255833398d0628502593163d01a
Sasha Lukas <sashamati@gmail.com>
2020-05-27 21:03:27 -0700

    starting the view Get

--- bugs/views.py
+++ bugs/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+def ticket(request, ticket_id):
+	ticket = Ticket.objects.get(pk=int(ticket_id))
+	if request.method == 'POST':
+
+		if form.is_valid():
+			ticket =  Ticket.objects.get(pk=int(ticket_id))
+
+
+				# recipient_list = (ticket.submitter_email, )
+				# send_mail(subject, message, from_email, recipient_list)
+			comment.save()
+            return render(request, "home.html")
---
+++ bugtracker/forms.py
@@ -0,0 +1,4 @@
+from django import forms
+from bugs.models import BugTracker
+
+
