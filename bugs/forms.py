a9a3401350ca566b6d101ea8f484296770a0dc48
Sasha Lukas <sashamati@gmail.com>
2020-05-28 15:27:46 -0700

    adding customUser

--- bugs/forms.py
+++ bugs/forms.py
@@ -1,14 +1,9 @@
 from django import forms
-from django.forms import ModelForm
+
+# from django.forms import ModelForm
 from .models import BugTracker
 
-class TicketForm(ModelForm):
-    class Meta:
-        model = BugTracker
-        fields = ['title', 'description', 'ticket_status']
-        # labels = {
-        #     'ticket_type': _('Ticket Type'),
-        # }
-        help_texts = {
-            'ticket_type': _('Please note: Feature request priority')
-        }
+
+class TicketForm(forms.Form):
+    title = forms.CharField(max_length=100)
+    details = forms.CharField(widget=forms.Textarea)
--- bugs/models.py
+++ bugs/models.py
@@ -2,11 +2,14 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
 class CustomUser(AbstractUser):
-    pass
+    first_name = models.CharField(max_length=20)
+    user_name = models.CharField(max_length=20)
+
     def __str__(self):
         return self.username
+
 
 class BugTracker(models.Model):
     """Trouble tickets"""
